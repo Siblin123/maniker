e19b58498a7ccb28f5821c4c47d838d3b8e6c091
jiho <wlgh7546@naver.com>
2025-07-06 23:52:24 +0900

    first commit

---
+++ js/js.js
@@ -0,0 +1,9 @@
+  var swiper = new Swiper(".mySwiper1", {
+      spaceBetween: 30,
+      effect: "fade",
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
